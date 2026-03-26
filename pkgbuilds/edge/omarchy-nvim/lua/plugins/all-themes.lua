33760762163a71d76c78434d2f050ca5f63c0f59
David Heinemeier Hansson <david@hey.com>
2026-03-26 16:33:43 +0100

    Update and add new default themes

--- pkgbuilds/edge/omarchy-nvim/lua/plugins/all-themes.lua
+++ pkgbuilds/edge/omarchy-nvim/lua/plugins/all-themes.lua
@@ -84,7 +84,12 @@ return {
 		priority = 1000,
 	},
 	{
-		"xero/miasma.nvim",
+		"OldJobobo/miasma.nvim",
+		lazy = true,
+		priority = 1000,
+	},
+	{
+		"omacom-io/lumon.nvim",
 		lazy = true,
 		priority = 1000,
 	},
